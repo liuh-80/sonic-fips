29b26f555dd4ec43aa274b7337ba543eba591bee
liuh-80 <liuh@microsoft.com>
2025-01-21 11:15:16 +0000

    Fix script

--- rules/krb5.mk
+++ rules/krb5.mk
@@ -1,13 +1,14 @@
 # krb5
 
-KRB5_VERSION_MAIN = 1.20.1-2
-KRB5_VERSION_FULL = $(KRB5_VERSION_MAIN)+deb12u2
-KRB5_VERSION_FIPS = $(KRB5_VERSION)+fips
+KRB5_VERSION_MAIN = 1.20.1
+KRB5_VERSION_FULL = $(KRB5_VERSION_MAIN)-2+deb12u2
+KRB5_VERSION_FIPS = $(KRB5_VERSION_FULL)+fips
 KRB5 = libk5crypto3_$(KRB5_VERSION_FIPS)_$(ARCH).deb
 $(KRB5)_SRC_PATH = $(SRC_PATH)/krb5
+KERB5_DST_PATH = krb5-$(KRB5_VERSION_MAIN)
 
 # Download krb5 code
-$(KRB5)_PRE_SCRIPT = rm -rf $(KRB5_SRC_PATH);dget -u http://deb.debian.org/debian/pool/main/k/krb5/krb5_$(KRB5_VERSION_FULL).dsc;mv ./krb5-$(KRB5_VERSION_MAIN) $(KRB5_SRC_PATH);
+$(KRB5)_PRE_SCRIPT = rm -rf $(KERB5_DST_PATH);rm -rf $(SRC_PATH)/krb5;dget -u http://deb.debian.org/debian/pool/main/k/krb5/krb5_$(KRB5_VERSION_FULL).dsc;mv $(KERB5_DST_PATH) $(SRC_PATH)/krb5;
 
 
 MAIN_TARGETS += $(KRB5)
